ae4ae9c436231c84559946ead93a20887970f2d6
ChamanSharmaReet1988 <chamantestapp@gmail.com>
2025-05-03 21:54:43 +0530

    added tab

---
+++ ResumeAI/Views/AIAssistantView.swift
@@ -0,0 +1,18 @@
+//
+//  AIAssistantView.swift
+//  ResumeAI
+//
+//  Created by Chaman on 03/05/25.
+//
+
+import SwiftUI
+
+struct AIAssistantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AIAssistantView()
+}
