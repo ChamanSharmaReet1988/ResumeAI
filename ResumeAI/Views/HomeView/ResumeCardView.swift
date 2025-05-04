66364f4cead1dca7548bf2379e4fc808c14c4e27
ChamanSharmaReet1988 <chamantestapp@gmail.com>
2025-05-04 17:02:30 +0530

    change structure

--- ResumeAI/Views/HomeView/ResumeCardView.swift
+++ ResumeAI/Views/HomeView/ResumeCardView.swift
@@ -5,8 +5,32 @@
 //  Created by Chaman on 04/05/25.
 //
 
-import UIKit
+import SwiftUI
 
-class ResumeCardView: NSObject {
+struct ResumeCardView: View {
+    var resume: Resume
 
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(resume.title ?? EmptyString)
+                .font(.headline)
+                .lineLimit(2)
+                .foregroundColor(.primary)
+
+            Text("Last edited: \(resume.lastEdited ?? EmptyString))")
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+        .frame(width: 200, height: 100)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(12)
+        .shadow(color: Color.black.opacity(0.05), radius: 3, x: 0, y: 2)
+    }
+
+    private func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter.string(from: date)
+    }
 }
---
+++ ResumeAI/Views/Tabbar/ResumeAIApp.swift
@@ -0,0 +1,18 @@
+//
+//  ResumeAIApp.swift
+//  ResumeAI
+//
+//  Created by Chaman on 22/02/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ResumeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
